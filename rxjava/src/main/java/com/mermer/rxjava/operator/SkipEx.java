c356a9150b74c6637d25cc389f7798f3d968265a
mermer <mincoln419@naver.com>
2022-09-22 16:15:46 +0900

    skip takeUntil

--- rxjava/src/main/java/com/mermer/rxjava/operator/SkipEx.java
+++ rxjava/src/main/java/com/mermer/rxjava/operator/SkipEx.java
@@ -7,24 +7,25 @@ import com.mermer.common.SampleData;
 
 import io.reactivex.Observable;
 
-public class TakeUntilEx {
+public class SkipEx {
 
 	public static void main(String[] args) throws InterruptedException {
 		
 		Observable.fromIterable(SampleData.carList)
-		.takeUntil((Car car) -> car.getCarName().equals("트랙스"))
+		.skip(3)
 		.subscribe(car -> System.out.println("car::" + car.getCarName()));
 		
 		System.out.println("==============================");
 		
 		
 		Observable.interval(1000L, TimeUnit.MILLISECONDS)
-			.takeUntil(Observable.timer(5500L, TimeUnit.MILLISECONDS))
+			.skip(3500L, TimeUnit.MILLISECONDS)
 			.subscribe(data -> System.out.println("data::" + data));
 			
 		
 			
 		Thread.sleep(6000L);
+		
 	}
 	
 }
