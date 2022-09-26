deeb1d29d4d74a92e27ef786389e629de1d6b398
mermer <mincoln419@naver.com>
2022-09-27 08:42:15 +0900

    Merge

---
+++ rxjava/src/main/java/com/mermer/rxjava/operator/coupled/MergeEx.java
@@ -0,0 +1,24 @@
+package com.mermer.rxjava.operator.coupled;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+
+public class MergeEx {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		Observable<Long> observable1 = Observable.interval(200L, TimeUnit.MILLISECONDS)
+				.take(5);
+		
+		Observable<Long> observable2 = Observable.interval(400L, TimeUnit.MILLISECONDS)
+				.take(5)
+				.map(num -> num + 1000);
+		
+		Observable.merge(observable1, observable2)
+		.subscribe(data -> System.out.println("data::" + data));
+		
+		Thread.sleep(4000L);
+		
+	}
+}
