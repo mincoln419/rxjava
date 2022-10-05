37f9817362bd7440a758d0b6a8540c181914bf16
mermer <mincoln419@naver.com>
2022-10-05 15:27:29 +0900

    single, trampoline

---
+++ rxjava/src/main/java/com/mermer/rxjava/scheduler/TrampolineEx.java
@@ -0,0 +1,27 @@
+package com.mermer.rxjava.scheduler;
+
+import com.mermer.common.util.LogType;
+import com.mermer.common.util.Logger;
+import com.mermer.common.util.TimeUtil;
+
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+
+public class TrampolineEx {
+	public static void main(String[] args) {
+		
+		Observable<String> observable = Observable.just("1","2","3","4","5");
+		
+		observable.subscribeOn(Schedulers.trampoline())
+			.map(data -> "##" + data + "##")
+			.subscribe(data -> Logger.log(LogType.ON_NEXT, data));
+		
+		
+		observable.subscribeOn(Schedulers.trampoline())
+		.map(data -> "$$" + data + "$$")
+		.subscribe(data -> Logger.log(LogType.ON_NEXT, data));
+		
+		TimeUtil.sleep(1000L);
+			
+	}
+}
