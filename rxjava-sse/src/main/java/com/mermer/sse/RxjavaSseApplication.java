d02d3415f29e658fca0f496c0297e5b53baeb5b7
mermer <mincoln419@naver.com>
2022-10-11 12:46:46 +0900

    rxjava sse project

---
+++ rxjava-sse/src/main/java/com/mermer/sse/RxjavaSseApplication.java
@@ -0,0 +1,13 @@
+package com.mermer.sse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxjavaSseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxjavaSseApplication.class, args);
+	}
+
+}
---
+++ rxjava-sse/src/test/java/com/mermer/sse/RxjavaSseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mermer.sse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxjavaSseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
